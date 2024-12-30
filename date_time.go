f1c4f20b189937ce19f5f4443e7af6d4e066b80b
Seeingu <seeingasu@gmail.com>
2024-12-30 11:46:21 +0800

    feat(datetime): init

---
+++ date_time.go
@@ -0,0 +1,78 @@
+package icu4xgo
+
+//#include <icu4xgo.h>
+//#include <stdlib.h>
+//#include <string.h>
+import "C"
+
+type DateTimeFormatter struct {
+	tzPtr        *C.IGTimeZoneInfo
+	datetimePtr  *C.IGDateTime
+	formatterPtr *C.IGZonedDateTimeFormatter
+	locale       *Locale
+	calendar     *C.IGCalendar
+}
+
+func NewDateTimeFormatter(l *Locale) *DateTimeFormatter {
+	f := &DateTimeFormatter{}
+	f.locale = l
+	f.calendar = C.ig_init_calendar(l.ptr)
+	return f
+}
+
+func (f *DateTimeFormatter) CalendarKind() int {
+	kind := C.ig_calendar_kind(f.calendar)
+	return int(kind)
+}
+
+func (f *DateTimeFormatter) TimeZoneId() string {
+	return C.GoString(C.ig_time_zone_info_id(f.tzPtr))
+}
+
+func (f *DateTimeFormatter) SetDateTime() *DateTimeFormatter {
+	codes := C.IGDateTimeCalendarCodes{
+		year:       2021,
+		month:      2,
+		day:        1,
+		hour:       0,
+		minute:     12,
+		second:     23,
+		nanosecond: 0,
+		calendar:   f.calendar,
+	}
+	f.datetimePtr = C.ig_init_datetime_from_codes(codes)
+	return f
+}
+
+func (f *DateTimeFormatter) SetTimeZone() *DateTimeFormatter {
+	f.tzPtr = C.ig_init_time_zone_info()
+	return f
+}
+
+type DateTimeLength int
+
+const (
+	DateTimeLengthLong DateTimeLength = iota
+	DateTimeLengthMedium
+	DateTimeLengthShort
+)
+
+func (f *DateTimeFormatter) SetFormatter(length DateTimeLength) *DateTimeFormatter {
+	f.formatterPtr = C.ig_init_zoned_date_time_formatter(f.locale.ptr, C.DateTimeLength(length))
+	return f
+}
+
+func (f *DateTimeFormatter) Format() string {
+	result := C.ig_datetime_format(f.formatterPtr, f.datetimePtr, f.tzPtr)
+	return C.GoString(result)
+}
+
+func (f *DateTimeFormatter) DayOfYear() int {
+	return int(C.icu4x_DateTime_day_of_year_mv1(f.datetimePtr.datetime))
+}
+
+func (f *DateTimeFormatter) Free() {
+	C.ig_free_time_zone_info(f.tzPtr)
+	C.ig_free_datetime(f.datetimePtr)
+	C.ig_free_zoned_date_time_formatter(f.formatterPtr)
+}
---
+++ ig_date_time.h
@@ -0,0 +1,67 @@
+#ifndef ICUGO_DATE_TIME_H
+#define ICUGO_DATE_TIME_H
+
+#include "./ig_locale.h"
+#include "./icu4x/DateTime.h"
+#include "./icu4x/TimeZoneInfo.h"
+#include "./icu4x/ZonedDateTimeFormatter.h"
+#include "./icu4x/DataProvider.h"
+#include "./icu4x/Calendar.h"
+
+#include <stdlib.h>
+
+typedef struct IGDateTime
+{
+    DateTime *datetime;
+} IGDateTime;
+
+typedef struct IGZonedDateTimeFormatter
+{
+    ZonedDateTimeFormatter *formatter;
+} IGZonedDateTimeFormatter;
+
+typedef struct IGTimeZoneInfo
+{
+    TimeZoneInfo *timezone;
+} IGTimeZoneInfo;
+
+typedef struct IGCalendar
+{
+    Calendar *calendar;
+} IGCalendar;
+
+typedef struct IGDateTimeCalendarCodes
+{
+    int32_t year;
+    uint8_t month;
+    uint8_t day;
+    uint8_t hour;
+    uint8_t minute;
+    uint8_t second;
+    uint32_t nanosecond;
+    IGCalendar *calendar;
+} IGDateTimeCalendarCodes;
+
+IGDateTime *ig_init_datetime_from_codes(IGDateTimeCalendarCodes codes);
+
+const char *ig_datetime_format(
+    IGZonedDateTimeFormatter *formatter,
+    IGDateTime *datetime,
+    IGTimeZoneInfo *timezone);
+
+IGZonedDateTimeFormatter *ig_init_zoned_date_time_formatter(const IGLocale *locale, DateTimeLength length);
+
+IGTimeZoneInfo *ig_init_time_zone_info(void);
+
+const char *ig_time_zone_info_id(IGTimeZoneInfo *tz);
+
+IGCalendar *ig_init_calendar(const IGLocale *locale);
+
+AnyCalendarKind ig_calendar_kind(IGCalendar *c);
+
+void ig_free_calendar(IGCalendar *calendar);
+void ig_free_zoned_date_time_formatter(IGZonedDateTimeFormatter *formatter);
+void ig_free_datetime(IGDateTime *datetime);
+void ig_free_time_zone_info(IGTimeZoneInfo *timezone);
+
+#endif // ICUGO_DATE_TIME_H
