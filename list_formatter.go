365b73024cc07f4c48cbfbd2c958a929013080fb
Seeingu <seeingasu@gmail.com>
2025-01-09 22:40:48 +0800

    feat(list_formatter): implement platform-specific list formatter for Darwin and general use

--- list_formatter.go
+++ list_formatter.go
@@ -1,18 +1,5 @@
 package icu4xgo
 
-//#include <icu4xgo.h>
-//#include <stdlib.h>
-//#include <string.h>
-import "C"
-
-import (
-	"runtime"
-)
-
-type ListFormatter struct {
-	ptr *C.IGListFormatter
-}
-
 type ListInitType int
 
 const (
@@ -29,23 +16,6 @@ const (
 	ListLengthNarrow
 )
 
-func NewListFormatter(locale *CLocale, initType ListInitType, listLength ListLength) *ListFormatter {
-	l := &ListFormatter{
-		ptr: C.ig_init_list_formatter(
-			locale.ptr,
-			C.IGListInitType(initType),
-			C.ListLength(listLength),
-		),
-	}
-	runtime.SetFinalizer(l, (*ListFormatter).free)
-	return l
-}
-
-func (l *ListFormatter) Format(items []string) string {
-	listView := arrayToDiplomatStrings(items)
-	return C.GoString(C.ig_list_format(l.ptr, listView))
-}
-
-func (l *ListFormatter) free() {
-	C.ig_free_list_formatter(l.ptr)
+type ListFormatter interface {
+	Format(items []string) string
 }
