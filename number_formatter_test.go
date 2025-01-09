62138a19f4c6f3e82dc3656e915c4716ae166279
Seeingu <seeingasu@gmail.com>
2025-01-09 16:11:56 +0800

    feat(number_formatter): darwin migration

--- number_formatter_test.go
+++ number_formatter_test.go
@@ -8,21 +8,24 @@ import (
 
 func TestNumberFormatter(t *testing.T) {
 	t.Run("NumberFormat in en-US", func(t *testing.T) {
-		locale := NewCLocale("en-US")
+		locale := NewLocale("en-US")
 		nf := NewNumberFormatter(locale)
-		f := nf.Format("123456.789")
+		f, err := nf.Format("123456.789")
+		assert.NoError(t, err)
 		assert.Equal(t, "123,456.789", f)
 	})
 	t.Run("NumberFormat in de-DE", func(t *testing.T) {
-		locale := NewCLocale("de-DE")
+		locale := NewLocale("de-DE")
 		nf := NewNumberFormatter(locale)
-		f := nf.Format("123456.789")
+		f, err := nf.Format("123456.789")
+		assert.NoError(t, err)
 		assert.Equal(t, "123.456,789", f)
 	})
 	t.Run("NumberFormat in ar-EG", func(t *testing.T) {
-		locale := NewCLocale("ar-EG")
+		locale := NewLocale("ar-EG")
 		nf := NewNumberFormatter(locale)
-		f := nf.Format("123456.789")
+		f, err := nf.Format("123456.789")
+		assert.NoError(t, err)
 		assert.Equal(t, "١٢٣٬٤٥٦٫٧٨٩", f)
 	})
 }
