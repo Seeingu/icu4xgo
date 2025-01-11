9aad3021cd1609864e796768cbcd88347d31180a
Seeingu <seeingasu@gmail.com>
2025-01-11 14:10:44 +0800

    feat(collator): general collator, add support for options

--- collator_test.go
+++ collator_test.go
@@ -1,8 +1,9 @@
 package icu4xgo
 
 import (
-	"runtime"
 	"testing"
+
+	"golang.org/x/text/collate"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -11,18 +12,23 @@ func TestCollator(t *testing.T) {
 	t.Run("Compare in en-GB", func(t *testing.T) {
 		c := NewCollator(NewLocale("en-GB"))
 		assert.Equal(t, -1, c.Compare("abc", "abd"))
-		// TODO(xxx): non universal comparison
-		if runtime.GOOS == "darwin" {
-			assert.Equal(t, -1, c.Compare("a", "ä"))
-		} else {
-			assert.Equal(t, 0, c.Compare("a", "ä"))
-		}
-		assert.Equal(t, 1, c.Compare("z", "ä"))
 	})
 
 	t.Run("Compare in sv", func(t *testing.T) {
 		c := NewCollator(NewLocale("sv"))
 		assert.Equal(t, -1, c.Compare("a", "ä"))
 		assert.Equal(t, -1, c.Compare("z", "ä"))
+	})
+
+	t.Run("Compare in de ", func(t *testing.T) {
+		c := NewCollator(NewLocale("de"))
+		assert.Equal(t, -1, c.Compare("a", "ä"))
+		assert.Equal(t, 1, c.Compare("z", "ä"))
+		assert.Equal(t, -1, c.Compare("z", "Z"))
+	})
+	t.Run("Compare in de ignore case", func(t *testing.T) {
+		c := NewCollatorWithOptions(NewLocale("de"), collate.IgnoreCase)
+		assert.Equal(t, 0, c.Compare("z", "Z"))
+		assert.Equal(t, 1, c.Compare("Z", "ä"))
 	})
 }
